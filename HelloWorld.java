7c121e193b7b1dc8ba9d7512ea6110018fa4465d
acalypha09 <fahroldhisukirno9@gmail.com>
2026-01-13 14:02:06 +0700

    menambahkan kelas baru

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,9 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Acalypha!");
+        System.out.println("Hello, Dunia!"); 
     }
+}
+
+class HelloAnimal{
+
 }
